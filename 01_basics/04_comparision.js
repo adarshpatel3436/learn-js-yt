a1b26aabc72c29e576601ad98b54bed0d8e07f6b
Adarsh Patel <161011841+adarshpatel3436@users.noreply.github.com>
2025-11-27 16:17:02 +0000

    comparison

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined >= 0);
+
+// === also checks the data type
+
+console.log("2" === 2);
+
+
+// is tarah ke comparison ko avoid karte h 
